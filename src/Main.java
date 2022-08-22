d1b2a25831a2639413003171a26a2a4bbd5d56b1
Chudov Ilya <chudow-iluha@mail.ru>
2022-08-22 20:29:26 +0300

    int changed on long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int [] sales = {25, 76, 78, 88};
+        long [] sales = {25, 76, 78, 88};
         SalesManager box = new SalesManager(sales);
         box.max();
         System.out.println(box.max());
